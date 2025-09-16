b3d3ccc8bd127848826661f6809b027c93ea88ac
Arvind Chaturvedi <138122453+ArvindChaturvedi@users.noreply.github.com>
2025-09-16 10:54:20 +0530

    Update Cmnd.sh

--- Cmnd.sh
+++ Cmnd.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 ZONE_ID=${1:-"YOUR_ZONE_ID"}
 
-echo "DNS Records Routing Traffic (CNAME + Alias Records)"
-echo "=================================================="
+echo "Record Name                    Record Type    TTL    Routes Traffic To"
+echo "============================================================================"
 
 aws route53 list-resource-record-sets --hosted-zone-id $ZONE_ID --output json | \
-jq -r '
-  ["Record Name", "Record Type", "TTL", "Routes Traffic To"],
-  (.ResourceRecordSets[] | 
-    if .Type == "CNAME" then 
-      [.Name, "CNAME", (.TTL | tostring), .ResourceRecords.Value] 
-    elif .Type == "A" and .AliasTarget then 
-      [.Name, "ALIAS (A)", "Auto-managed", .AliasTarget.DNSName] 
-    else empty end
-  ) | @tsv
-' | column -t -s $'\t'
+jq -r '.ResourceRecordSets[] | 
+  if .Type == "CNAME" and (.ResourceRecords | length > 0) then 
+    [.Name, "CNAME", (.TTL | tostring), .ResourceRecords.Value] 
+  elif .Type == "A" and .AliasTarget then 
+    [.Name, "ALIAS", "Auto", .AliasTarget.DNSName] 
+  else empty end | 
+  @tsv' | \
+column -t -s $'\t'
